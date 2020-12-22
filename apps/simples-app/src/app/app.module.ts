f6016c461fab44353cd4a954b2a8e50cd1233cf2
Carlos Casa <duardbr@gmail.com>
2020-12-22 15:31:44 -0300

    adicionando o shared material

--- apps/simples-app/src/app/app.module.ts
+++ apps/simples-app/src/app/app.module.ts
@@ -1,13 +1,23 @@
+import 'hammerjs';
+
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
+    MaterialModule,
+  ],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
